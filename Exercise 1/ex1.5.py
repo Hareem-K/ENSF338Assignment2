f712afee13ec08d62fdc4ab7884cb36d356a44d1
Hareem Khan <hareem.khan@ucalgary.ca>
2023-02-10 17:10:21 -0700

    part 8

--- Exercise 1/ex1.5.py
+++ Exercise 1/ex1.5.py
@@ -7,3 +7,53 @@ This program times the original code and our improved version, for all integers 
 and plots the results.
 
 '''
+
+import time
+import matplotlib.pyplot as plt
+
+#original given function
+def original_func(n):
+    if n == 0 or n == 1:
+        return n
+
+    else:
+        return original_func(n-1) + original_func(n-2)
+
+#improved function from part 6    
+def improved_func(n):
+    def func_memo(n, m):
+
+        if n in m:
+            return m[n]
+
+        n_number = func_memo(n-1, m) + func_memo(n-2, m)
+        m[n] = n_number
+        return n_number
+
+    m = {0: 1, 1: 1}
+    return func_memo(n, m)
+
+# Time the original function
+original_times = []
+for i in range(36):
+    start = time.time()
+    original_func(i)
+    end = time.time()
+    original_times.append(end - start)
+    
+# Time the improved function
+improved_times = []
+for i in range(36):
+    start = time.time()
+    improved_func(i)
+    end = time.time()
+    improved_times.append(end - start)
+
+# Plot the results
+plt.plot(original_times, label='Original Code', color='darkmagenta')
+plt.plot(improved_times, label='Improved Code', color='lightcoral')
+plt.xlabel('Input (n)')
+plt.ylabel('Time (s)')
+plt.title("Time Comparison of Original and Improved Code")
+plt.legend()
+plt.show()
