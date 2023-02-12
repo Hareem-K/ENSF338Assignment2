c203f37d6d122d2aac34889b9c4af993ce033ac3
Hareem Khan <hareem.khan@ucalgary.ca>
2023-02-11 17:52:46 -0700

    added exercise 2 folder

---
+++ Exercise 2/ex2.2.py
@@ -0,0 +1,10 @@
+'''
+Filename: ex2.2.py
+Course: ENSF 338
+Group Number: 12
+
+This program tests the code on all the inputs at:
+https://raw.githubusercontent.com/ldklab/ensf338w23/main/assignments/assignment2/ex2.json
+and plots timing results.
+
+'''
